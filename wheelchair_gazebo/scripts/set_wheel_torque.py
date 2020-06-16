6f421a7c69e4a165378c2a7b4c48b9366e8c8d12
Mahsa-Khalili <mahsakhalili@gmail.com>
2020-06-16 00:36:40 -0700

    modified service client to read torque from a csv file

--- wheelchair_gazebo/scripts/set_wheel_torque.py
+++ wheelchair_gazebo/scripts/set_wheel_torque.py
@@ -2,8 +2,16 @@
 
 import rospy
 import time
+import os
 
 from gazebo_msgs.srv import *
+import rospkg
+import csv
+
+rospack = rospkg.RosPack()
+pkg_path = rospack.get_path('wheelchair_gazebo')
+
+filename = os.path.join(pkg_path, 'scripts/StraightF_T1_WS64_Mahsa.csv')
 
 if __name__ == '__main__':
 
@@ -11,17 +19,32 @@ if __name__ == '__main__':
 
 	rospy.wait_for_service('/gazebo/apply_joint_effort')
 
-	time.sleep(10)
+	time.sleep(5)
 
 	apply_joint_effort = rospy.ServiceProxy('/gazebo/apply_joint_effort', ApplyJointEffort)
 
-	joint_name = 'WheelR_cont'
-	effort = 46.0
 	start_time = rospy.Duration.from_sec(0)
 	duration = rospy.Duration.from_sec(20)
 
-	try:
-		resp1 = (apply_joint_effort(joint_name, effort, start_time, duration))
+	with open(filename, 'rb') as csvfile:
+		csvfile.readline()
+		test_read = csv.reader(csvfile, delimiter=',')
+		i = 0
+		for data in test_read:
+			i+=1
+			print(data[5], data[6])
+			row_fl_left = float(data[5])
+			row_fl_right = float(data[6])
 
-	except rospy.ServiceException, e:
-		print "Sercice did not process"
+			effort_left = 0.01*row_fl_left
+			effort_right = 0.01*row_fl_right
+
+			try:
+				resp1 = (apply_joint_effort('WheelL_cont', effort_left, start_time, duration))
+				resp2 = (apply_joint_effort('WheelR_cont', effort_right, start_time, duration))
+
+				time.sleep(1/300)
+
+			except rospy.ServiceException, e:
+				print "Sercice did not process"
+		print(i)
