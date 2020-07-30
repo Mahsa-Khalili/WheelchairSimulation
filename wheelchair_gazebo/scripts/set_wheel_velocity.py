86cb9fd33ec5077d52fde6ab0ce1104ba9300d12
Mahsa-Khalili <mahsakhalili@gmail.com>
2020-07-30 01:31:37 -0700

    updated joint torque recording & added wheel_velocity_controller

--- wheelchair_gazebo/scripts/set_wheel_velocity.py
+++ wheelchair_gazebo/scripts/set_wheel_velocity.py
@@ -3,40 +3,52 @@
 import rospkg
 import os
 import rospy
+from std_msgs.msg import Float64
 import csv
 import time
 
 rospack = rospkg.RosPack()
 pkg_path = rospack.get_path('wheelchair_gazebo')
-
 filename = os.path.join(pkg_path, 'scripts/StraightF_T1_WS64_Mahsa.csv')
 
-
-def WheelVelControl():
+def VelocityControl():
 	# initialize node
-	rospy.init_node('LeftWheel_AngVel_Pub')	
-	rospy.init_node('RightWheel_AngVel_Pub')	
+	rospy.init_node('velocity_publisher')	
 
-	# start twist publisher
-	angvelL_pub = rospy.Publisher('', ..., queue_size = 1)
-	angvelR_pub = rospy.Publisher('', ..., queue_size = 1)
+	# advertise velocity publisher
+	velocity_left_pub = rospy.Publisher('/WheelL_velocity_controller/command', Float64, queue_size = 1)
+	velocity_right_pub = rospy.Publisher('/WheelR_velocity_controller/command', Float64, queue_size = 1)
+
+	# setting publishing rate in Hz
+	rate = rospy.Rate(240)
 
 	with open(filename, 'rb') as csvfile:
 		csvfile.readline()
 		data = csv.reader(csvfile, delimiter=',')
-
+		# rowcount = sum(1 for row in data)
+		# print(rowcount)
+		
 		for row in data:
 			print(row[1], row[2])
-			angvel_L = float(row[1])
-			angvel_R = float(row[2]
+			left_vel = float(row[1])
+			right_vel = float(row[2])
 
 			# publish twist command to wheelchair
-			twist_pub.publish(move_cmd)
+			velocity_left_pub.publish(left_vel)
+			velocity_right_pub.publish(right_vel)
 
-			time.sleep(0.004167)
+			rate.sleep()
+
+		left_vel = 0.0
+		right_vel = 0.0
+
+		while not rospy.is_shutdosn():
+			velocity_left_pub.publish(left_vel)
+			velocity_right_pub.publish(right_vel)
+			rate.sleep()
 
 if __name__ == '__main__':
 	try:
-		WheelVelControl()
+		VelocityControl()
 	except:
-		"There was an error publihing angular velocity messages"
+		"There was an error publihing twist messages"
